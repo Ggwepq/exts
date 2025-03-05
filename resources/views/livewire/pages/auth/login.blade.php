0a29e55096f512ea51f5616669ff22364f3ad6ca
Cedjuani <ggwepq@gmail.com>
2025-03-06 00:52:01 +0800

    🎨 style(ETS-2): improve login page ui

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -5,8 +5,7 @@ use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
-{
+new #[Layout('layouts.guest')] class extends Component {
     public LoginForm $form;
 
     /**
@@ -28,44 +27,74 @@ new #[Layout('layouts.guest')] class extends Component
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form wire:submit="login">
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
+    <div class="min-h-screen bg-base-200 flex items-center">
+        <div class="card mx-auto w-full max-w-5xl shadow-xl">
+            <div class="grid md:grid-cols-2 grid-cols-1 bg-base-100 rounded-xl">
+                <!-- Left Side (Landing Intro) -->
+                <div>
+                    <livewire:pages.auth.land-intro />
+                </div>
+
+                <!-- Right Side (Login Form) -->
+                <div class="py-10 px-10">
+                    <h2 class="text-2xl font-semibold mb-2 text-center">Log In</h2>
+
+                    <form wire:submit="login">
+                        <div class="mb-4">
+                            <!-- Email -->
+                            <div class="form-control w-full mt-4">
+                                <x-input-label for="email" :value="__('Email')" />
+                                <input type="email" wire:model="form.email"
+                                    class="input input-bordered validator w-full" name="email" required
+                                    autocomplete="username" />
+                                <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
+                            </div>
+
+                            <!-- Password -->
+                            <div class="form-control w-full mt-4">
+                                <x-input-label for="password" :value="__('Password')" />
+                                <input wire:model="form.password" id="password"
+                                    class="input input-bordered validator w-full" type="password" name="password"
+                                    required autocomplete="new-password" />
+                                <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
+                            </div>
+
+                        </div>
+
+                        <!-- Remember Me -->
+                        <div class="block mt-4">
+                            <label for="remember" class="inline-flex items-center">
+                                <input wire:model="form.remember" id="remember" type="checkbox" class="checkbox"
+                                    name="remember">
+                                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                            </label>
+                        </div>
+
+                        <!-- Forgot Password Link -->
+                        <div class="text-right text-primary">
+                            <a href="{{ route('password.request') }}"
+                                class="text-sm text-primary inline-block hover:text-secondary hover:underline hover:cursor-pointer transition duration-200">
+                                Forgot Password?
+                            </a>
+                        </div>
+
+                        <!-- Submit Button -->
+                        <button type="submit" class="btn mt-2 w-full btn-primary"
+                            wire:loading.class="loading loading-dots loading-sm">
+                            Login
+                        </button>
+
+                        <!-- Register Link -->
+                        <div class="text-center mt-4">
+                            Don't have an account?
+                            <a href="{{ route('register') }}"
+                                class="inline-block text-primary hover:text-secondary hover:underline hover:cursor-pointer transition duration-200">
+                                Register
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember" class="inline-flex items-center">
-                <input wire:model="form.remember" id="remember" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}" wire:navigate>
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </div>
