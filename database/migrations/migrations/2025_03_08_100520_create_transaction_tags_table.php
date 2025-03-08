747f4cedbe25a92c62b7cd8251d20d2c1b82da65
Alt-62 <seanhayes897@gmail.com>
2025-03-08 18:50:17 +0800

    Update 2025_03_08_100520_create_transaction_tags_table.php
    Signed-off-by: Alt-62 <seanhayes897@gmail.com>

--- database/migrations/migrations/2025_03_08_100520_create_transaction_tags_table.php
+++ database/migrations/migrations/2025_03_08_100520_create_transaction_tags_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('transaction_tags', function (Blueprint $table) {
             $table->id();
             $table->foreignId('transaction_id')->constrained('transactions')->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade');
             $table->timestamps();
         });
     }
