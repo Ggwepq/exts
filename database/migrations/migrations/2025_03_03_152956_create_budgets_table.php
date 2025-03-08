67920445011ef91b1a226134800badbeae5ab2ce
Alt-62 <seanhayes897@gmail.com>
2025-03-08 18:27:12 +0800

    Add files via upload
    Signed-off-by: Alt-62 <seanhayes897@gmail.com>

---
+++ database/migrations/migrations/2025_03_03_152956_create_budgets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('transaction_categories')->onDelete('cascade');
+            $table->decimal('target_amount', 15, 2);
+            $table->decimal('spent_amount', 15, 2)->default(0);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status', ['Active', 'Expired']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('budgets');
+    }
+};
