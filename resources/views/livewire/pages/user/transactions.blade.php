c3ecf880cfefc39ce6c1e8c2874c22c3ff2be7c8
Cedjuani <ggwepq@gmail.com>
2025-03-15 09:56:20 +0800

    refactor: remove mount() and format code

--- resources/views/livewire/pages/user/transactions.blade.php
+++ resources/views/livewire/pages/user/transactions.blade.php
@@ -3,30 +3,6 @@
 use Livewire\Volt\Component;
 
 new #[Layout('layouts.app')] class extends Component {
-    public $transactions;
-
-    public function mount()
-    {
-        /*
-         * Fetch user tranasction based on date
-         */
-        $this->transactions = auth()
-            ->user()
-            ->transactions()
-            ->orderBy('created_at', 'DESC')
-            ->get()
-            ->groupBy(function ($transaction) {
-                $date = \Carbon\Carbon::parse($transaction->created_at);
-
-                return $date->format('F j, Y');
-            })
-            ->all();
-    }
-
-    public function placeholder()
-    {
-        return view('livewire.pages.user.components.placeholder');
-    }
 }; ?>
 
 <div x-data="{ isOpen: false }">
