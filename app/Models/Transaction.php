195cbfc06d6b820680e8bc2d2f396b010b1413b3
Cedjuani <ggwepq@gmail.com>
2025-04-02 13:01:14 +0800

    feat: create tag if not existing
    style tag component
    non-existing tags can be created

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -35,7 +35,7 @@ class Transaction extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class, 'transaction_tags', 'transaction_id', 'tag_id');
+        return $this->belongsToMany(Tag::class, 'transaction_tags');
     }
 
     public function transactionCategories()
