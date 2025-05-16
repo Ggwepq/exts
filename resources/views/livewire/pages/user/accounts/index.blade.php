4aaeed851c9d57d167514e0eb9eae68764695038
cedjuani <ggwepq@gmail.com>
2025-05-17 00:59:29 +0800

    style: retain margin when sidebar is open

--- resources/views/livewire/pages/user/accounts/index.blade.php
+++ resources/views/livewire/pages/user/accounts/index.blade.php
@@ -48,10 +48,13 @@ new class extends Component {
         $total = $expense . '/' . $income;
     }
 }; ?>
-
 <section>
-    <div class="transition-all duration-300 ease-in-out"
-        :class="{ 'md:mr-[17rem] lg:mr-[23rem] xl:mr-[27rem] 2xl:mr-[41rem]': detailSidebarOpen }">
+    <!-- Yes Margin -->
+    <!-- Yes Margin -->
+    <!-- <div class="transition-all duration-300 ease-in-out" -->
+    <!--     :class="{ 'md:mr-[17rem] lg:mr-[23rem] xl:mr-[27rem] 2xl:mr-[41rem]': detailSidebarOpen }"> -->
+    <!-- No Margin -->
+    <div class="transition-all duration-300 ease-in-out">
         @livewire('pages.user.containers.main-header', ['component' => 'pages.user.accounts.header'])
         <div class="flex-1 overflow-y-auto md:pt-4 pt-4 px-6 bg-base-200 h-screen">
             <div class="card w-full p-6 bg-base-100 shadow-xl mt-2">
