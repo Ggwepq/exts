3da6e942b04bf3dc6e2c93b2e5ddd8824bb8554c
Alt-62 <seanhayes897@gmail.com>
2025-05-28 01:13:24 +0800

    Income and Expense Dashboard Chart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,7 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body class="font-roboto antialiased">
